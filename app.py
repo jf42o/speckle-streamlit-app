847a3f543778b080d338dea758031ef545698747
Jannik Fath <jafath@web.de>
2023-04-09 11:50:42 +0200

    M

--- app.py
+++ app.py
@@ -430,6 +430,7 @@ if not LOCAL:
             <div class="center-container"></div>
             <div class="right-container">
                 <div class="nav-links">
+                    <a href=specklelit.streamlit.app/?access_code={access_code}">Home</a>
                     <a href="specklelit.streamlit.app/Data?access_code={access_code}">Data</a>
                     <a href="specklelit.streamlit.app/About?access_code={access_code}">About</a>
                 </div>
