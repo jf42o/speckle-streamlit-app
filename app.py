b45882c31b84c52784f8c16d5174c8d3e90122a3
jf42o <jafath@web.de>
2023-04-30 10:26:13 +0200

    M

--- app.py
+++ app.py
@@ -60,8 +60,6 @@ def login():
         st.session_state['refresh_token'] = None
     try:
         access_code = st.experimental_get_query_params()['access_code'][0]
-        if access_code:
-                access_code = st.experimental_set_query_params(**{'access_code' : 100})
         st.session_state['access_code'] = access_code
     except:
         access_code = None
